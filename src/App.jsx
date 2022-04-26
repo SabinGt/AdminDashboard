8013e3b100362dfd055f3478bdd880c4ffaa08cf
Sabin Gautam <sabingt0@gmail.com>
2022-04-26 12:22:48 +0545

    add the navbar

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Dashboard from './components/Dashboard';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div class="wrapper">
+      <Header />
+      <Menu />
+      <Dashboard />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
